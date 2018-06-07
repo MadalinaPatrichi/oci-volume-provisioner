4d74a3545bd0f95a69ea4e409c9f66eee8817217
MadalinaPatrichi <ioana-madalina.patrichi@oracle.com>
2018-06-07 16:10:19 +0100

    Fix for retrieving availability domain

--- test/system/runner.py
+++ test/system/runner.py
@@ -743,10 +743,10 @@ def _main():
                             "demooci-no-ad-" + test_id, args['check_oci'])
 
         _log("Running system test: Create volume with FSS", as_banner=True)
-        _availability_domain = _get_terraform_output_var(terraform_env, TERRAFORM_AVAILABILITY_DOMAIN)
+        _availability_domain = _get_terraform_output_var(_get_terraform_env(), TERRAFORM_AVAILABILITY_DOMAIN)
         _test_create_volume(compartment_id,
                             _create_yaml("../../manifests/example-claim-fss.template", test_id, _get_region()),
-                            "demooci-fss-" + test_id, args['check_oci'], availability_domain=availability_domain,
+                            "demooci-fss-" + test_id, args['check_oci'], availability_domain=_availability_domain,
                             storageType=FS_STORAGE)
         _log("Running system test: Create volume from backup", as_banner=True)
         if args['check_oci']: 
